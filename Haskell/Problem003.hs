ff8d1d36e60b46b6a945e54d9e44ce84f39fbcc1
RecursiveLogic <qrx9823@gmail.com>
2016-09-21 12:16:22 -0400

    Minor syntax change

--- Haskell/Problem003.hs
+++ Haskell/Problem003.hs
@@ -3,6 +3,6 @@ primeFactors x =
     case factors of
         [] -> [x]
         _  -> factors ++ primeFactors (div x (head factors))
-    where factors  = take 1 $ filter (\y -> mod x y == 0) [2..x - 1]
+    where factors = take 1 $ filter (\y -> mod x y == 0) [2..x - 1]
 
 main = print (last $ primeFactors 600851475143)
