699fc702fce052811f95ce52f2fbd5f223a80ed3
RecursiveLogic <luis@radiux.io>
2017-04-18 18:21:48 -0400

    Updated answer to problem 008 for Haskell

--- Haskell/Problem008.hs
+++ Haskell/Problem008.hs
@@ -3,4 +3,5 @@ import Data.List
 
 main = do
   content <- readFile "Haskell/Problem008.txt"
-  print (maximum [product $ map ((read . (:[]))) b | b <- map (take 13) . filter ((>12) . length) $ (tails . concat $ splitOn "\n" content)])
+  print (maximum [product $ map ((read . (:[]))) b | b <- map (take 13) .
+    filter ((>12) . length) $ (tails . concat $ splitOn "\n" content)])
